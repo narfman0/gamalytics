3e4597b886174918fc04a615d8e181c724b32dbe
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-11-14 03:38:48 +0000

    -Adding current status, created db and first hellow world view
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@685 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

---
+++ gamalytics/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from gamalytics.models import Game,Rating,User
+
+admin.site.register(Game)
+admin.site.register(Rating)
+admin.site.register(User)
---
+++ gamalytics/models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    joined = models.DateTimeField(default=datetime.now)
+
+class Rating(models.Model):
+    username = models.CharField(max_length=100)
+    gamename = models.CharField(max_length=100)
+    tag = models.CharField(max_length=100)
+    value = models.FloatField(validators = [MinValueValidator(0), MaxValueValidator(100)],default=50)
+    time = models.DateTimeField(default=datetime.now)
+
+class Game(models.Model):
+    gamename = models.CharField(max_length=100)
+    metacritic = models.URLField()
+    gametrailers = models.URLField()
+    description = models.TextField()
