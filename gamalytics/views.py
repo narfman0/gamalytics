fe91ebb999ce27111bd1efcece797c8db047afd5
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-11-27 20:02:23 +0000

    -Add index.html template
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@691 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- gamalytics/views.py
+++ gamalytics/views.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from gamalytics.models import Game
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the gamalytics index.")
+  games=Game.objects.filter()
+  context={'games':games}
+  return render(request,'index.html',context)
 
 def detail(request, gamename):
-    game=Game.objects.filter(gamename=gamename)[0]
-    context = {'game': game}
-    return render(request,'game.html',context)
+  game=Game.objects.filter(gamename=gamename)[0]
+  context={'game': game}
+  return render(request,'game.html',context)
