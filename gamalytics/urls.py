cfd7b0d075a55d55a951b3fb19c3c0eb5a71035c
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-11-27 20:54:13 +0000

    -Adding tags to game template. Add /g for game paths.
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@692 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- gamalytics/urls.py
+++ gamalytics/urls.py
@@ -4,6 +4,6 @@ from gamalytics import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<gamename>\w+)/$', views.detail, name='detail'),
+    url(r'^g/(?P<gamename>\w+)/$', views.detail, name='detail'),
 )
 
--- gamalytics/views.py
+++ gamalytics/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from gamalytics.models import Game
+from gamalytics.models import Game,Rating
 
 def index(request):
-  games=Game.objects.filter()
+  games=Game.objects.all()
   context={'games':games}
   return render(request,'index.html',context)
 
 def detail(request, gamename):
   game=Game.objects.filter(gamename=gamename)[0]
-  context={'game': game}
+  ratingMap={}
+  for rating in Rating.objects.filter(gamename=gamename):
+    s=ratingMap.get(rating.tag,[])
+    s.append(rating.value)
+    ratingMap[rating.tag]=s
+  ratings={}
+  for k,v in ratingMap.items():
+    ratings[k]=sum(v)/len(v)
+  sortedRatings=sorted(ratings.items(), key=lambda x: x[1], reverse=True)
+  context={'game' : game, 'ratings' : sortedRatings}
   return render(request,'game.html',context)
