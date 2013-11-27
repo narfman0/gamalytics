ef4aaa1e103c9ff024aa3f7a068d65b48228da26
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-11-27 16:53:25 +0000

    -Adding specific game views
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@689 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- gamalytics/urls.py
+++ gamalytics/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from gamalytics import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<gamename>\w+)/$', views.detail, name='detail'),
 )
 
--- gamalytics/views.py
+++ gamalytics/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the gamalytics index.")
+
+def detail(request, gamename):
+    return HttpResponse("You're looking at game %s." % gamename)
