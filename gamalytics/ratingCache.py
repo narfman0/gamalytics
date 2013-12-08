344caf5acfb40ca0059d3a35d112c546aee29f47
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-12-08 22:13:15 +0000

    -Remove obnoxious rating cache dicitonary
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@750 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- gamalytics/ratingCache.py
+++ gamalytics/ratingCache.py
@@ -5,11 +5,7 @@ from gamalytics.models import Game,Rating
 from django.core.cache import cache
 from hashlib import md5
 
-#Stupid memcached. This class checks if memcached is active, if so, uses it.
-#Otherwise it uses a local map.
 class RatingCache:
-  memcachedActive=2
-  tagCache={}
   #prepopulate - if true, calculate all games at startup
   def __init__(self, prepopulate):
     if prepopulate:
@@ -17,10 +13,7 @@ class RatingCache:
       current=1.0
       for game in Game.objects.all():
         tags=self.calculateGameAveragedTags(game.name)
-        if self.isMemcachedActive():
-          cache.set(self.getKey(game.name), tags, 0)
-        else:
-          self.tagCache[game.name]=tags
+        cache.set(self.getKey(game.name), tags, 0)
         print('Cache '+str(100*current/count) + '% done, finished '+game.name)
         current += 1
 
@@ -38,25 +31,10 @@ class RatingCache:
       ratings[k]=sum(v)/len(v)
     return sorted(ratings.items(), key=lambda x: x[1], reverse=True)
   
-  def isMemcachedActive(self):
-    if self.memcachedActive == 2:
-      key='ismemcachedactivekey'
-      cache.set(key,"value",0)
-      if cache.get(key) == None:
-        self.memcachedActive = 0
-      else:
-        self.memcachedActive = 1
-    return self.memcachedActive == 1
-
   def getGameTagsAveraged(self, name):
-    if self.isMemcachedActive():
-      key=self.getKey(name)
-      result=cache.get(key)
-      if result is None:
-        result=self.calculateGameAveragedTags(name)
-        cache.set(key, result, 60*10)
-    else:
-      if name not in self.tagCache:
-        self.tagCache[name]=self.calculateGameAveragedTags(name)
-      result=self.tagCache[name]
+    key=self.getKey(name)
+    result=cache.get(key)
+    if result is None:
+      result=self.calculateGameAveragedTags(name)
+      cache.set(key, result, 60*10)
     return result
