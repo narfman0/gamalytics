bcbb8a1e926acb8fd7524b4d6c50f39889573b98
narfman0 <narfman0@f34f5d6a-d8fa-4a85-8d7d-3577574e89da>
2013-12-09 00:35:17 +0000

    -Add to STATIC_ROOT
    git-svn-id: file:///media/data/other/svn/repos/gamalytics/trunk@753 f34f5d6a-d8fa-4a85-8d7d-3577574e89da

--- gamalytics/settings.py
+++ gamalytics/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_PATH + '/../static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -118,6 +118,7 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
+        #'LOCATION': 'pub-memcache-14392.us-east-1-1.1.ec2.garantiadata.com:14392',
     },
 }
 
